8a442be2dcd75be2b2c9142d14b54fb5446ea85d
Joey Geiger <jgeiger@gmail.com>
2010-09-16 11:33:36 -0700

    rails default, add filters for db, autotest and vendor/bundle This way we're not testing every gem

--- lib/simplecov/adapters.rb
+++ lib/simplecov/adapters.rb
@@ -40,6 +40,9 @@ SimpleCov.adapters.define 'rails' do
   add_filter '/features/'
   add_filter '/spec/'
   add_filter '/config/'
+  add_filter '/db/'
+  add_filter '/autotest/'
+  add_filter '/vendor/bundle/'
   
   add_group 'Controllers', 'app/controllers'
   add_group 'Models', 'app/models'
