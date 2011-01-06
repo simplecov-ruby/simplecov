f512e5ef6333815fd8fbc449d03e06447c2b3166
Christoph Olszowka <christoph@olszowka.de>
2011-01-06 18:48:34 +0100

    Make sure adapter definitions go through on 1.8

--- test/test_1_8_fallbacks.rb
+++ test/test_1_8_fallbacks.rb
@@ -2,6 +2,10 @@ require 'helper'
 
 # Tests that verify that on 1.8 versions of ruby, simplecov simply
 # does not launch and does not cause errors on the way
+#
+# TODO: This should be expanded upon all methods that could potentially
+# be called in a test/spec-helper simplecov config block
+#
 class Test18FallBacks < Test::Unit::TestCase
   on_ruby '1.8' do
     should "return false when calling SimpleCov.start" do
@@ -14,6 +18,16 @@ class Test18FallBacks < Test::Unit::TestCase
     
     should "return false when calling SimpleCov.configure with a block" do
       assert_equal false, SimpleCov.configure { raise "Shouldn't reach this!?" }
+    end
+    
+    should "allow to define an adapter" do
+      begin
+        SimpleCov.adapters.define 'testadapter' do
+          add_filter '/config/'
+        end
+      rescue => err
+        assert false, "Adapter definition should have been fine, but raised #{err}"
+      end
     end
   end
 end
