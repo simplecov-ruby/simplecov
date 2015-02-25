d66de666343853eefe3de8a9fe45d65dea303521
Erik Michaels-Ober <sferik@gmail.com>
2015-02-25 09:42:14 +0100

    Add some helpful singleton methods to the version string

--- lib/simplecov/version.rb
+++ lib/simplecov/version.rb
@@ -1,3 +1,22 @@
 module SimpleCov
   VERSION = "0.9.2"
+  def VERSION.to_a
+    split('.').map { |part| part.to_i }
+  end
+
+  def VERSION.major
+    to_a[0]
+  end
+
+  def VERSION.minor
+    to_a[1]
+  end
+
+  def VERSION.patch
+    to_a[2]
+  end
+
+  def VERSION.pre
+    to_a[3]
+  end
 end
