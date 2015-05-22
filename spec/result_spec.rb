16ca0632b1ad28008876dd4f398bbfe7fa5fbf6a
Paul Tashman <paul.tashman@careerbuilder.com>
2015-05-22 00:56:15 -0400

    Adds tests

--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -50,7 +50,11 @@ describe "result" do
         expect(subject.covered_percent).to eq(86.66666666666667)
       end
 
-      [:covered_percent, :covered_strength, :covered_lines, :missed_lines, :total_lines].each do |msg|
+      it "has accurate covered percentages" do
+        expect(subject.covered_percentages).to eq([80.0, 80.0, 100.0])
+      end
+
+      [:covered_percent, :covered_percentages, :covered_strength, :covered_lines, :missed_lines, :total_lines].each do |msg|
         it "responds to #{msg}" do
           expect(subject).to respond_to(msg)
         end
@@ -72,6 +76,10 @@ describe "result" do
 
           it "has the same covered_percent" do
             expect(dumped_result.covered_percent).to eq(subject.covered_percent)
+          end
+
+          it "has the same covered_percentages" do
+            expect(dumped_result.covered_percentages).to eq(subject.covered_percentages)
           end
 
           it "has the same timestamp" do
@@ -100,6 +108,10 @@ describe "result" do
 
       it "has 80 covered percent" do
         expect(SimpleCov::Result.new(original_result).covered_percent).to eq(80)
+      end
+
+      it "has [80.0, 80.0] covered percentages" do
+        expect(SimpleCov::Result.new(original_result).covered_percentages).to eq([80.0, 80.0])
       end
     end
 
