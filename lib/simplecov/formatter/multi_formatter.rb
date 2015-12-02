simplecov-ruby/simplecov

commit ddde04d4c9e2359ae7dcfdf27a247c61b238cb07

Use new formatters= syntax internally

lib/simplecov/formatter/multi_formatter.rb
@@ -1,27 +1,31 @@
 module SimpleCov
   module Formatter
     class MultiFormatter
-      def self.[](*args)
-        Class.new(self) do
+      module InstanceMethods
+        def format(result)
+          formatters.map do |formatter|
+            begin
+              formatter.new.format(result)
+            rescue => e
+              STDERR.puts("Formatter #{formatter} failed with #{e.class}: #{e.message} (#{e.backtrace.first})")
+              nil
+            end
+          end
+        end
+      end
+
+      def self.new(formatters = nil)
+        Class.new do
           define_method :formatters do
-            @formatters ||= args
+            @formatters ||= Array(formatters)
           end
+          include InstanceMethods
         end
       end
 
-      def format(result)
-        formatters.map do |formatter|
-          begin
-            formatter.new.format(result)
-          rescue => e
-            STDERR.puts("Formatter #{formatter} failed with #{e.class}: #{e.message} (#{e.backtrace.first})")
-            nil
-          end
-        end
-      end
-
-      def formatters
-        @formatters ||= []
+      def self.[](*args)
+        warn "#{Kernel.caller.first}: [DEPRECATION] ::[] is deprecated. Use ::new instead."
+        new(*args)
       end
     end
   end

features/config_formatters.feature
@@ -30,7 +30,7 @@ Feature:
     Given SimpleCov for Test/Unit is configured with:
       """
       require 'simplecov'
-      SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      SimpleCov.formatters = [
         SimpleCov::Formatter::SimpleFormatter,
         Class.new do
           def format(result)

lib/simplecov/configuration.rb
@@ -80,7 +80,7 @@ module SimpleCov
     # Sets the configured formatters.
     #
     def formatters=(formatters)
-      @formatter = SimpleCov::Formatter::MultiFormatter[*formatters]
+      @formatter = SimpleCov::Formatter::MultiFormatter.new(formatters)
     end
 
     #

test/test_result.rb
@@ -128,7 +128,7 @@ class TestResult < Minitest::Test # rubocop:disable Metrics/ClassLength
 
       context "and multi formatter being used" do
         setup do
-          SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+          SimpleCov.formatters = [
             SimpleCov::Formatter::SimpleFormatter,
             SimpleCov::Formatter::SimpleFormatter,
           ]

commit d40d545e31f98af05ad55d7db37dfdfa3d260acf

Fixed MultiFormatter.[] behaviour, which was broken in ddde04d.

lib/simplecov/formatter/multi_formatter.rb
@@ -25,7 +25,7 @@ module SimpleCov
 
       def self.[](*args)
         warn "#{Kernel.caller.first}: [DEPRECATION] ::[] is deprecated. Use ::new instead."
-        new(*args)
+        new(Array([*args]))
       end
     end
   end