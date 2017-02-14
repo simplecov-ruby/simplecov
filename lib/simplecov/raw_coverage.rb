6dafc25ad591d21a479c8e35b2659ca429c2f981
Adam Roben <adam@roben.org>
2017-02-14 14:51:55 -0500

    Fix RuboCop offenses

--- lib/simplecov/raw_coverage.rb
+++ lib/simplecov/raw_coverage.rb
@@ -1,6 +1,6 @@
 module SimpleCov
   module RawCoverage
-    module_function
+  module_function
 
     # Merges multiple Coverage.result hashes
     def merge_results(*results)
@@ -10,26 +10,28 @@ module SimpleCov
     end
 
     # Merges two Coverage.result hashes
-    def merge_resultsets(a, b)
-      (a.keys | b.keys).each_with_object({}) do |filename, merged|
-        result1 = a[filename]
-        result2 = b[filename]
-        merged[filename] = merge_file_coverage(result1, result2)
+    def merge_resultsets(result1, result2)
+      (result1.keys | result2.keys).each_with_object({}) do |filename, merged|
+        file1 = result1[filename]
+        file2 = result2[filename]
+        merged[filename] = merge_file_coverage(file1, file2)
       end
     end
 
-    def merge_file_coverage(a, b)
-      unless a && b
-        return (a || b).dup
-      end
+    def merge_file_coverage(file1, file2)
+      return (file1 || file2).dup unless file1 && file2
 
-      a.zip(b).map do |count1, count2|
-        sum = count1.to_i + count2.to_i
-        if sum.zero? && (count1.nil? || count2.nil?)
-          nil
-        else
-          sum
-        end
+      file1.zip(file2).map do |count1, count2|
+        merge_line_coverage(count1, count2)
+      end
+    end
+
+    def merge_line_coverage(count1, count2)
+      sum = count1.to_i + count2.to_i
+      if sum.zero? && (count1.nil? || count2.nil?)
+        nil
+      else
+        sum
       end
     end
   end
