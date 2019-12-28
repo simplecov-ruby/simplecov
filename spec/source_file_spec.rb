c8514e336c32b3c1fdf6ee7afe9faf10c62f4536
Tobias Pfeiffer <pragtob@gmail.com>
2019-12-28 15:51:20 +0100

    Case basic spec

--- spec/source_file_spec.rb
+++ spec/source_file_spec.rb
@@ -372,4 +372,43 @@ describe SimpleCov::SourceFile do
       end
     end
   end
+
+  context "a file with a case statement" do
+    COVERAGE_FOR_CASE_STATEMENT_RB = {
+      :lines =>
+        [1, 1, 1, nil, 0, nil, 1, nil, 0, nil, 0, nil, nil, nil],
+      :branches => {
+        [:case, 0, 3, 4, 12, 7] => {
+          [:when, 1, 5, 6, 5, 10] => 0,
+          [:when, 2, 7, 6, 7, 10] => 1,
+          [:when, 3, 9, 6, 9, 10] => 0,
+          [:else, 4, 11, 6, 11, 11] => 0
+        }
+      }
+    }.freeze
+
+    subject do
+      SimpleCov::SourceFile.new(source_fixture("case.rb"), COVERAGE_FOR_CASE_STATEMENT_RB)
+    end
+
+    describe "line coverage" do
+      it "covers 4/7" do
+        expect(subject.relevant_lines).to eq 7
+        expect(subject.covered_lines.size).to eq 4
+        expect(subject.missed_lines.size).to eq 3
+      end
+    end
+
+    describe "branch coverage" do
+      it "covers 1/4" do
+        expect(subject.total_branches.size).to eq 4
+        expect(subject.covered_branches.size).to eq 1
+        expect(subject.missed_branches.size).to eq 3
+      end
+
+      it "covers the branch that includes 42" do
+        expect(subject.branches_report[6]).to eq [[1, "+"]]
+      end
+    end
+  end
 end
