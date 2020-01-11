e2c3b131c04e254773fbad2f079220446830bec0
Tobias Pfeiffer <pragtob@gmail.com>
2020-01-11 14:25:16 +0100

    make spec_helper injectable to be able to test branch coverage in the same project

--- test_projects/parallel_tests_project/spec/spec_helper.rb
+++ test_projects/parallel_tests_project/spec/spec_helper.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
-require "simplecov"
-
-SimpleCov.start
+# We're injecting simplecov_config via aruba in cucumber here
+# depending on what the test case is...
+begin
+  require File.join(File.dirname(__FILE__), "simplecov_config")
+rescue LoadError
+  warn "No SimpleCov config file found!"
+end
 
 require_relative "../lib/all"
