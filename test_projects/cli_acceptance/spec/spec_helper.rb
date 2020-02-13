b15895934a70240af0579edd7c4acbc38c41d615
Justin Webster <justin.webster@integnology.com>
2020-02-13 10:49:44 -0800

    [simplecov#853] Added additional (commented out) code showing how to reproduce issue #853.
    Note: Since this reproduction involves the use of an external "community" formatter, I left this code commented out for now to avoid introducing a new (and cyclic) gem dependency.

--- test_projects/cli_acceptance/spec/spec_helper.rb
+++ test_projects/cli_acceptance/spec/spec_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 require "simplecov"
+# require 'simplecov-cobertura'
 require_relative "../lib/cli_acceptance"
+
+# BUG: simplecov#853: Uncomment the following line (and the `require` above on line 4), to reproduce issue #853.
+# Note that when the following line is uncommented, the `using ...` list at the bottom of the HTML report index shows fewer reports being merged, and lower coverage numbers.
+# SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
