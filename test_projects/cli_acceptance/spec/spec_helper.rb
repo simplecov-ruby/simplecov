f34a77bfa22a1e022cdcbe63666c1bd6756716e9
Justin Webster <justin.webster@integnology.com>
2020-02-18 10:34:15 -0800

    [simplecov#853] Successfully reproduced issue #853 using only a SimpleFormatter (and also with CoberturaFormatter as a commented-out alterative).
    Note: Since the CoberturaFormatter is an external "community" formatter, I left the code that uses it commented out to avoid introducing a new (and cyclic) gem dependency.

--- test_projects/cli_acceptance/spec/spec_helper.rb
+++ test_projects/cli_acceptance/spec/spec_helper.rb
@@ -4,6 +4,18 @@ require "simplecov"
 # require 'simplecov-cobertura'
 require_relative "../lib/cli_acceptance"
 
-# BUG: simplecov#853: Uncomment the following line (and the `require` above on line 4), to reproduce issue #853.
-# Note that when the following line is uncommented, the `using ...` list at the bottom of the HTML report index shows fewer reports being merged, and lower coverage numbers.
-# SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+# BUG: simplecov#853: Uncomment any 1 of the following '# BUG' lines (and the `require` above on line 4 if needed), to reproduce issue #853.
+# Note that when a '# BUG' line is uncommented, the `using ...` list at the bottom of the HTML report index shows fewer reports being merged, and lower coverage numbers.
+# SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter # merges properly
+SimpleCov.formatter = SimpleCov::Formatter::SimpleFormatter # BUG: simplecov#853: merges incorrectly
+# SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter # BUG: simplecov#853: merges incorrectly
+# SimpleCov.formatters = Array(SimpleCov.formatter) # merges properly
+# SimpleCov.formatters = Array(SimpleCov.formatters) # merges properly
+# SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter] # merges properly
+# SimpleCov.formatters = Array(SimpleCov.formatters) + [SimpleCov::Formatter::SimpleFormatter] # merges properly
+# SimpleCov.formatters = Array(SimpleCov.formatters) + SimpleCov::Formatter::CoberturaFormatter # merges properly
+# SimpleCov.formatters = [SimpleCov::Formatter::SimpleFormatter] # BUG: simplecov#853: merges incorrectly
+# SimpleCov.formatters = [SimpleCov::Formatter::CoberturaFormatter] # BUG: simplecov#853: merges incorrectly
+# SimpleCov.formatters = [SimpleCov::Formatter::CoberturaFormatter, SimpleCov::Formatter::HTMLFormatter] # merges properly
+
+# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(SimpleCov.formatters)
