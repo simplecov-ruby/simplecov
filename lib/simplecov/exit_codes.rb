385e8d932957d7aec6c05ac8f5e3f721b2d3b449
Tobias Pfeiffer <pragtob@gmail.com>
2020-08-09 20:56:17 +0200

    Refactor result_exit_status
    This method was giant spaghetti, that was hard to maintain and
    fix stuff in.
    
    The new solution might seem overblown but with it's current
    interdependencies was the best I could come up with.
    
    Decided to decouple it a bit from asking SimpleCov itself for
    the coverage values to make it easier to test in isolation. No
    tests added as of right now though since it's tested quite a bit.
    
    The strategy to put each check into its own class produces more
    lines of code but makes their intent clearer and also makes it
    easier to figure out what belongs to what check.

--- lib/simplecov/exit_codes.rb
+++ lib/simplecov/exit_codes.rb
@@ -8,3 +8,8 @@ module SimpleCov
     MAXIMUM_COVERAGE_DROP = 3
   end
 end
+
+require_relative "exit_codes/exit_code_handling"
+require_relative "exit_codes/maximum_coverage_drop_check"
+require_relative "exit_codes/minimum_coverage_by_file_check"
+require_relative "exit_codes/minimum_overall_coverage_check"
---
+++ lib/simplecov/exit_codes/maximum_coverage_drop_check.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module SimpleCov
+  module ExitCodes
+    class MaximumCoverageDropCheck
+      def initialize(result, maximum_coverage_drop, covered_percent)
+        @result = result
+        @maximum_coverage_drop = maximum_coverage_drop
+        @covered_percent = covered_percent
+      end
+
+      def failing?
+        return false unless maximum_coverage_drop && last_run
+
+        coverage_diff > maximum_coverage_drop
+      end
+
+      def report
+        $stderr.printf(
+          "Coverage has dropped by %<drop_percent>.2f%% since the last time (maximum allowed: %<max_drop>.2f%%).\n",
+          drop_percent: coverage_diff,
+          max_drop: maximum_coverage_drop
+        )
+      end
+
+      def exit_code
+        SimpleCov::ExitCodes::MAXIMUM_COVERAGE_DROP
+      end
+
+    private
+
+      attr_reader :result, :maximum_coverage_drop, :covered_percent
+
+      def last_run
+        return @last_run if defined?(@last_run)
+
+        @last_run = SimpleCov::LastRun.read
+      end
+
+      def coverage_diff
+        raise "Trying to access coverage_diff although there is no last run" unless last_run
+
+        @coverage_diff ||= last_run[:result][:covered_percent] - covered_percent
+      end
+    end
+  end
+end
