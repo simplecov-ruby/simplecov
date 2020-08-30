ed2bbf74adf88565ddee9974adc036f3fb041603
Federico Moyá <federico.moya@wyeworks.com>
2020-08-30 20:30:17 -0300

    Address rubocop's offenses

--- lib/simplecov/default_formatter.rb
+++ lib/simplecov/default_formatter.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 DEFAULT_FORMATTER = "HTML"
 
 configured_formatters = ENV.fetch("SIMPLECOV_FORMATTERS") { DEFAULT_FORMATTER }
 configured_formatters = configured_formatters.split(",")
 configured_formatters.map! do |f|
-  SimpleCov::Formatter.const_get("#{f}Formatter") rescue nil
+  SimpleCov::Formatter.const_get("#{f}Formatter")
+rescue StandardError
+  nil
 end
 configured_formatters.compact
 
