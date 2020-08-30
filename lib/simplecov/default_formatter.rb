a22be13b1eaaf81f07d27e81ff405542abccb759
Federico Moyá <federico.moya@wyeworks.com>
2020-08-30 20:47:19 -0300

    Output warning when unrecognized format detected

--- lib/simplecov/default_formatter.rb
+++ lib/simplecov/default_formatter.rb
@@ -7,9 +7,10 @@ configured_formatters = configured_formatters.split(",")
 configured_formatters.map! do |f|
   SimpleCov::Formatter.const_get("#{f}Formatter")
 rescue StandardError
+  warn "SimpleCov #{f} format unrecognized"
   nil
 end
-configured_formatters.compact
+configured_formatters.compact!
 
 if configured_formatters.count > 1
   SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(configured_formatters)
