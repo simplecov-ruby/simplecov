ad232038d08bf215bbac03858257be62eb4d55c1
Federico Moyá <federico.moya@wyeworks.com>
2020-09-07 15:22:11 -0300

    Add unit tests

--- lib/simplecov/default_formatter.rb
+++ lib/simplecov/default_formatter.rb
@@ -7,7 +7,7 @@ module SimpleCov
       def from_env
         formatters = [SimpleCov::Formatter::HTMLFormatter]
 
-        # When running under a CI that uses CodeClimate JSON output is expected
+        # When running under a CI that uses CodeClimate, JSON output is expected
         if ENV.fetch("CC_TEST_REPORTER_ID", nil)
           require "simplecov_json_formatter"
           formatters.push(SimpleCov::Formatter::JSONFormatter)
