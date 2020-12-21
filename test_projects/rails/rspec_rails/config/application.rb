498f539edd39ba10a648fb2cd9862e052ab8be62
Geremia Taglialatela <tagliala.dev@gmail.com>
2020-12-21 20:40:32 +0100

    Update dummy Rails application to 6.1
    At the moment the Gemfile was updated, selenium-webdriver and webdrivers
    gems did not work with Ruby 3.0. Since they are not needed to run
    SimpleCov's specs, they have been commented out
    
    Adds `platforms: %I[jruby] to activerecord-jdbcsqlite3-adapter to
    allow installation of versions greater than 51.0

--- test_projects/rails/rspec_rails/config/application.rb
+++ test_projects/rails/rspec_rails/config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -22,11 +22,14 @@ Bundler.require(*Rails.groups)
 module RspecRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
