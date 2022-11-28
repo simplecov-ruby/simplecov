1cc7650454fc7c53e7a724941662c3e9d3329ccc
Yusuke Endoh <mame@ruby-lang.org>
2022-11-28 20:50:35 +0900

    Avoid double Coverage.start
    Since Ruby 3.1, Coverage.start raises an exception if the coverage
    measurement is already started.
    
    ```
    $ ruby -v -rcoverage -e 'Coverage.start; Coverage.start'
    ruby 3.1.3p185 (2022-11-24 revision 1a6b16756e) [x86_64-linux]
    -e:1:in `start': coverage measurement is already setup (RuntimeError)
            from -e:1:in `<main>'
    ```
    
    This should fix the issue #1020

--- lib/simplecov.rb
+++ lib/simplecov.rb
@@ -342,7 +342,7 @@ module SimpleCov
       if coverage_start_arguments_supported?
         start_coverage_with_criteria
       else
-        Coverage.start
+        Coverage.start unless Coverage.running?
       end
     end
 
@@ -351,7 +351,7 @@ module SimpleCov
         [lookup_corresponding_ruby_coverage_name(criterion), true]
       end.to_h
 
-      Coverage.start(start_arguments)
+      Coverage.start(start_arguments) unless Coverage.running?
     end
 
     CRITERION_TO_RUBY_COVERAGE = {
