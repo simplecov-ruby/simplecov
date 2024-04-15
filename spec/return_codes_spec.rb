99c004029b71118a2650c280f008144b60776202
Benoit Daloze <eregontp@gmail.com>
2024-04-15 20:58:23 +0200

    Add TruffleRuby in CI (#1079)
    * Add TruffleRuby in CI
    
    * Remove continue-on-error in unstable.yml workflow
    
    * This is a daily job, the failures only appear in the Actions tab so
      there seems to be little value to hide it.
    
    * Add comment about running only rake spec on truffleruby

--- spec/return_codes_spec.rb
+++ spec/return_codes_spec.rb
@@ -34,6 +34,8 @@ describe "return codes" do
         end
 
         it "prints a message to STDERR" do
+          # https://github.com/oracle/truffleruby/issues/3535
+          skip "fails on truffleruby" if RUBY_ENGINE == "truffleruby" && command.include?("testunit_bad.rb")
           expect(@stderr).to match(/stopped.+SimpleCov.+previous.+error/i)
         end
       end
